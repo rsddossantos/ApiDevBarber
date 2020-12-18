96b7c9a65e712c67eef94765df653aaac3439b3c
rodrigo.silveira <rodrigo.silveira@zanthus.com.br>
2020-12-18 00:37:21 -0300

    - Método de criação de usuário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Users;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['create', 'login']]);
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['error' => ''];
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if($validator->fails()) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $password = $request->input('password');
+            // Criando o usuário, validar email antes
+            $emailExists = User::where('email', $email)->count();
+            if($emailExists === 0) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->save();
+            } else {
+                $array['error'] = 'E-mail já cadastrado';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Dados incorretos';
+            return $array;
+        }
+        return $array;
+    }
 }
